f32bf75223c7b7ba4eeba4bca8e35d0638c721f3
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-03-26 15:54:17 +0530

    comment 3

--- firstnodejs/index.js
+++ firstnodejs/index.js
@@ -2,8 +2,21 @@ const express=require("express");
 
 const app=express();
 
+//get
 app.get("/se",(req,res)=>{
-    return res.json("Hello world");
+    return res.json("get request");
+});
+
+app.put("/se",(req,res)=>{
+    return res.json("put request");
+});
+
+app.post("/se",(req,res)=>{
+    return res.json("post request");
+});
+
+app.delete("/se",(req,res)=>{
+    return res.json("delete request");
 });
 
 const PORT=process.env.PORT || 5000;
