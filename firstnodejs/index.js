43d7f9f3842ed82693857ecd2ac98e2972647f43
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-03-26 15:40:37 +0530

    get

--- firstnodejs/index.js
+++ firstnodejs/index.js
@@ -2,6 +2,10 @@ const express=require("express");
 
 const app=express();
 
+app.get("/se",(req,res)=>{
+    return res.json("Hello world");
+});
+
 const PORT=process.env.PORT || 5000;
 
 app.listen(PORT, ()=> {
